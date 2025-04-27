d9465722fdb4c8206a81bf3942f2fddd62e7d7d7
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-27 14:16:51 -0700

    modify sumAll to add functionality engaged all tests in sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!(Number.isInteger(a) &&  Number.isInteger(b) && a > 0 && b > 0)) {
+        return "ERROR";
+    }
 
+    // formula n(n+1)/2 sums 1 to n
+    // n: nighest number - lowest number
+    // offsetTotal: (lowest number - 1) * n
+
+    let n = (a>b ? a-b : b-a) + 1;
+    let offsetTotal = ((a<b ? a : b) - 1) * n;
+
+    return (n*(n+1)/2) + offsetTotal;
 };
 
 // Do not edit below this line
