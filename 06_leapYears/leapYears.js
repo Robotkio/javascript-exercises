66b42a00aa0e9d14ae3fa67cdf8fc04fdaa86558
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-27 14:27:49 -0700

    add leapYear functionality enable all leapYear tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year%4)) {
+        if (!(year%100)) {
+            if (!(year%400)) {
+                // divisible by 4 and 400
+                return true;
+            }
+            // divisible by 4 and 100 but not 400
+            return false;
+        }
+        // divisible by 4 and not 100
+        return true;
+    }
+    // not divisible by 4
+    return false;
 };
 
 // Do not edit below this line
