0f40c4bf41dab1f600056ea85bfa2f1667e7012c
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-27 00:53:03 -0700

    modify reverseString to meet assing 02 reqs enable all tests in reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let halfWay = Math.floor(splitString.length/2);
+    for (let a = 0; a < halfWay; a++) {
+        let temp = splitString[a];
+        let b = splitString.length-1-a;
+        splitString[a] = splitString[b];
+        splitString[b] = temp;
+    }
+    return splitString.join("");
 };
 
 // Do not edit below this line
