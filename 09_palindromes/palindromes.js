b061132275ac8f3e67461f18c89f9e09ab73b2f8
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-30 22:12:53 -0700

    add functionality to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.split("")
+        .filter((char) => ![",", ".", " ", "!"].includes(char))
+        .map((char) => char.toLowerCase())
+    return arr.join("") == arr.toReversed().join("");
 };
 
 // Do not edit below this line
