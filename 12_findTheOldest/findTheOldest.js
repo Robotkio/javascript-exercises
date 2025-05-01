9a0a39d47dc7bb6e1ae748bc20b66303305b95b1
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-30 23:04:53 -0700

    duurrrr id dids thestufs

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let currentYear = new Date().getFullYear();
+    let oldestPerson = null;
+    let oldestAge = 0;
+
+    for (let person of people) {
+        let latestYearAlive = ("yearOfDeath" in person) ? person.yearOfDeath : currentYear;
+        let currentPersonsAge = latestYearAlive - person.yearOfBirth;
+
+        if (currentPersonsAge > oldestAge) {
+            oldestAge = currentPersonsAge;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
