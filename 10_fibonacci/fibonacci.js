4c1237c1586b080e3e00f010f48fa39ca1141202
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-30 22:32:42 -0700

    enable tests, add function to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(digit) {
+    if (digit < 0) { return "OOPS"; }
+    if (digit == 0) { return 0; }
 
+    let prev = 0;
+    let curr = 1;
+
+    for (let i=1; i<digit; i++) {
+        [prev, curr] = [curr, prev + curr];
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
