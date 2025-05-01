d1303325299662128fc3a0d38048956f413b1b0f
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-30 21:38:36 -0700

    filled in functionality and enabled tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) { return 0; }
+	return arr.reduce((total, val) => total + val);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) { return 0; }
+  return arr.reduce((total, val) => total * val);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	let total = 1;
+  while (val > 1){ total *= val--; }
+  return total;
 };
 
 // Do not edit below this line
